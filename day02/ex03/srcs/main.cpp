df488b63eb7f3dfef8fd565018c98e82b86449e9
Riyaz USMANOV <jannabel@e2r12p10.clusters.42paris.fr>
2022-11-04 13:08:23 +0100

    yeah

--- day02/ex03/srcs/main.cpp
+++ day02/ex03/srcs/main.cpp
@@ -2,11 +2,11 @@
 
 int main( void ) 
 {
-    std::cout << std::to_string(bsp(Point(2, 1), Point(6, 3), Point(2, 4), Point(3, 3))) << std::endl;
+    std::cout << (bsp(Point(2, 1), Point(6, 3), Point(2, 4), Point(3, 3))) << std::endl;
 
-    std::cout << std::to_string(bsp(Point(2, 1), Point(6, 3), Point(2, 4), Point(1, 1))) << std::endl;
+    std::cout << (bsp(Point(2, 1), Point(6, 3), Point(2, 4), Point(1, 1))) << std::endl;
 
-    std::cout << std::to_string(bsp(Point(2, 1), Point(6, 3), Point(2, 4), Point(4, 2.5))) << std::endl;
+    std::cout << (bsp(Point(2, 1), Point(6, 3), Point(2, 4), Point(4, 2.5))) << std::endl;
 
-    std::cout << std::to_string(bsp(Point(2, 1), Point(6, 3), Point(2, 4), Point(2, 4))) << std::endl;
+    std::cout << (bsp(Point(2, 1), Point(6, 3), Point(2, 4), Point(2, 4))) << std::endl;
 }
