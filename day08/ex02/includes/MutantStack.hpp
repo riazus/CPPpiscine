7cf41942c13855df324d57e7f577b06e0be86ecf
Riyaz Usmanov <usmanov.riyaz22@gmail.com>
2022-11-18 21:30:16 +0300

    ALL DONE

---
+++ day08/ex02/includes/MutantStack.hpp
@@ -0,0 +1,35 @@
+#include 	<stack>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack(void) 
+        {
+        }
+        
+        MutantStack(const MutantStack<T>& src) : std::stack<T>(src)
+        {
+        }
+        
+        MutantStack<T>& operator=(const MutantStack<T>& rhs)
+        {
+            this->c = rhs.c;
+            return *this;
+        }
+
+        ~MutantStack()
+        {
+        }
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        iterator begin() 
+        {
+            return this->c.begin();
+        }
+        iterator end() 
+        {
+            return this->c.end();
+        }
+};
