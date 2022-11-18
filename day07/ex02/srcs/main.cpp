a533a3d7d70fd18292b14f2c57b8166f2e14b64f
Riyaz Usmanov <usmanov.riyaz22@gmail.com>
2022-11-18 21:48:13 +0300

    small fixes

--- day07/ex02/srcs/main.cpp
+++ day07/ex02/srcs/main.cpp
@@ -26,6 +26,8 @@ int main()
 	for (unsigned int i = 0; i < b.size(); i++)
 		std::cout << a[i] << "\t" << b[i] << "\t" << c[i] << "\t" << d[i] << std::endl;
 
+	std::cout << "----------------------------------" << std::endl;
+
 	for (unsigned int i = 0; i < a.size(); i++)
 		a[i] = a[i] * 2;
 
